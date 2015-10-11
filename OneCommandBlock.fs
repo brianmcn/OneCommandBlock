94b3bbcfaebf2168f1e1a4c2ac73c7da995172bd
brianmcn <lorgon1@yahoo.com>
2015-10-11 19:36:00 -0400

    insta-placement

--- OneCommandBlock.fs
+++ OneCommandBlock.fs
@@ -135,6 +135,7 @@ let cmdsline = // TODO make so player places 2 spawn eggs, it computes dx dy dz,
 
 let cmds = 
     [|
+        yield "MINECART BLOCKS"
         // computes the dx/dy/dz from A to B (provided it's in within -127...127) 
         let MAX = 128
         let STEPS = [-128;-64;-32;-16;-8;-4;-2;-1]
@@ -170,8 +171,8 @@ let cmds =
 
 
 
-let escape(s:string) = s.Replace("\"","\\\"")
-let escape2(s) = escape(s).Replace("\"","^").Replace("\\","\\\\").Replace("^","\\\"")
+let escape(s:string) = s.Replace("\"","^").Replace("\\","\\\\").Replace("^","\\\"")    //    "  \    ->    \"   \\
+let escape2(s) = escape(escape(s))
 
 let BLOCKS = true
 
@@ -179,7 +180,7 @@ let BLOCKS = true
 [<System.STAThreadAttribute>]
 do
     let mutable sb = new System.Text.StringBuilder()
-    if cmds.[0] <> "MINECART" then
+    if not(cmds.[0].StartsWith("MINECART")) then
 #if RIDING        
         sb.Append("""summon Item ~ ~10 ~ {Item:{id:dirt,Damage:0,Count:1},Age:5999,""") |> ignore
         for c in cmds do
@@ -204,11 +205,11 @@ do
             elif c = "P" then
                 sb.Append("{id:FallingSand,Block:repeating_command_block,Time:1,Passengers:[") |> ignore
             elif c.StartsWith("O ") then
-                sb.Append(sprintf "{id:FallingSand,Block:command_block,Time:1,TileEntityData:{Command:\"%s\"},Passengers:[" (escape (c.Substring(2)))) |> ignore
+                sb.Append(sprintf "{id:FallingSand,Block:command_block,Time:1,TileEntityData:{Command:\"%s\"},Passengers:[" (escape2 (c.Substring(2)))) |> ignore
             elif c.StartsWith("C ") then
-                sb.Append(sprintf "{id:FallingSand,Block:chain_command_block,Time:1,Data:8,TileEntityData:{Command:\"%s\"},Passengers:[" (escape (c.Substring(2)))) |> ignore
+                sb.Append(sprintf "{id:FallingSand,Block:chain_command_block,Time:1,Data:8,TileEntityData:{Command:\"%s\"},Passengers:[" (escape2 (c.Substring(2)))) |> ignore
             else
-                sb.Append(sprintf "{id:FallingSand,Block:chain_command_block,Time:1,TileEntityData:{Command:\"%s\"},Passengers:[" (escape c)) |> ignore
+                sb.Append(sprintf "{id:FallingSand,Block:chain_command_block,Time:1,TileEntityData:{Command:\"%s\"},Passengers:[" (escape2 c)) |> ignore
         for i = 0 to cmds.Length do
             sb.Append("""]}""") |> ignore
 #endif
@@ -229,8 +230,21 @@ do
         for i = 0 to cmds.Length+3 do
             sb.Append("""}""") |> ignore
 #else
+        if cmds.[0].StartsWith("MINECART BLOCKS") then
+            for i = 1 to cmds.Length-1 do
+                let dy = 10 + cmds.Length - i 
+                if cmds.[i] = "R" then
+                    cmds.[i] <- sprintf "setblock ~ ~%d ~ redstone_block" dy
+                elif cmds.[i] = "P" then
+                    cmds.[i] <- sprintf "setblock ~ ~%d ~ repeating_command_block" dy
+                elif cmds.[i].StartsWith("O ") then
+                    cmds.[i] <- sprintf "setblock ~ ~%d ~ command_block 0 replace {Command:\"%s\"}" dy (escape (cmds.[i].Substring(2)))
+                elif cmds.[i].StartsWith("C ") then
+                    cmds.[i] <- sprintf "setblock ~ ~%d ~ chain_command_block 8 replace {auto:1b,Command:\"%s\"}" dy (escape (cmds.[i].Substring(2)))
+                else 
+                    cmds.[i] <- sprintf "setblock ~ ~%d ~ chain_command_block 0 replace {auto:1b,Command:\"%s\"}" dy (escape cmds.[i])
         // runs all cmds in 1 tick, 8 ticks after activation, leaves only original block
-        sb.Append(sprintf """summon FallingSand ~ ~0.55 ~ {Block:command_block,Time:1,TileEntityData:{ Command:"summon MinecartCommandBlock ~ ~1.5 ~ {Command:\"%s\",Passengers:[""" (escape2 cmds.[1])) |> ignore
+        sb.Append(sprintf """summon FallingSand ~ ~0.55 ~ {Block:command_block,Time:1,TileEntityData:{Command:"summon MinecartCommandBlock ~ ~1.5 ~ {Command:\"%s\",Passengers:[""" (escape2 cmds.[1])) |> ignore
         for i = 2 to cmds.Length-1 do
             sb.Append(sprintf """{id:MinecartCommandBlock,Command:\"%s\"},""" (escape2 cmds.[i])) |> ignore
         sb.Append("""{id:MinecartCommandBlock,Command:\"setblock ~ ~-2 ~ command_block 0 0 {auto:1b,Command:\\\"fill ~ ~ ~ ~ ~2 ~ air\\\"}\"},{id:MinecartCommandBlock,Command:\"kill @e[type=MinecartCommandBlock,r=5]\"}]}"},Passengers:[{id:FallingSand,Block:redstone_block,Time:1,Passengers:[{id:FallingSand,Block:activator_rail,Time:1}]}]}""") |> ignore
