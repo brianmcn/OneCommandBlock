d62bdefd3846d81ed437afbd43506a1b1fb6f98a
brianmcn <lorgon1@yahoo.com>
2015-10-12 09:39:53 -0400

    multiple columns, init code and block code

--- OneCommandBlock.fs
+++ OneCommandBlock.fs
@@ -25,7 +25,7 @@ let cmdsxx =
         """tellraw @a ["9  ",{"score":{"name":"Ticks","objective":"S"}}]"""
     |]
 
-let cmdsline = // TODO make so player places 2 spawn eggs, it computes dx dy dz, draws line
+let cmdsDrawLine = // TODO make so player places 2 spawn eggs, it computes dx dy dz, draws line
     [|
         // assuming DX DY and DZ are set in lineDraw objective, then this draws the line from player to relative (DX,DY,DZ) once player's lineDraw becomes non-zero
         yield "R"
@@ -133,7 +133,7 @@ let cmdsline = // TODO make so player places 2 spawn eggs, it computes dx dy dz,
         yield """tellraw @a {"text":"Initializing, wait one moment...","color":"red"}"""
     |]
 
-let cmds = 
+let cmdsComputeDxDyDz = 
     [|
         yield "MINECART BLOCKS"
         // computes the dx/dy/dz from A to B (provided it's in within -127...127) 
@@ -167,18 +167,79 @@ let cmds =
         yield "kill @e[tag=lineDrawTX]"
         yield "kill @e[tag=lineDrawTY]"
         yield "kill @e[tag=lineDrawTZ]"
+        // TODO kill linedrawab
+        // TODO call draw
+    |]
+
+let cmdsCircle = OldContraptions.drawCircle 
+
+let cmds = cmdsCircle
+let dummy = 
+    [|
+        "MINECART BLOCKS"
+        "R"
+        "P"
+        // if A placed, and player demands second endpoint, then place it and run the tool
+        "execute @e[tag=lineDrawA] ~ ~ ~ scoreboard players test @p endpoint 1 *"
+        "C execute @p ~ ~ ~ summon ArmorStand ~ ~ ~ {Marker:1,NoGravity:1,Tags:[\"lineDrawB\"]}"
+        "C blockdata X Y Z {auto:1b}"  // TODO coords - call out to compute dxdydz
+        "C blockdata X Y Z {auto:0b}"
+        // if A not yet placed, and player demands an endpoint, place A"
+        "testfor @e[tag=lineDrawA]"
+        "testforblock ~ ~1 ~ chain_command_block -1 {SuccessCount:0}"
+        "C scoreboard players test @p endpoint 1 *"
+        "C execute @p ~ ~ ~ summon ArmorStand ~ ~ ~ {Marker:1,NoGravity:1,Tags:[\"lineDrawA\"]}"
+        // clear endpoint request
+        "scoreboard players set @p endpoint 0"
+        // TODO make barrier particles
+        "R"
+        "O"
+        "scoreboard objectives add endpoint dummy"  // TODO clean up objectives   // TODO move objectives out of blocks, into just minecart commands
     |]
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TODO below does not work if '^' is in the original text
 let escape(s:string) = s.Replace("\"","^").Replace("\\","\\\\").Replace("^","\\\"")    //    "  \    ->    \"   \\
 let escape2(s) = escape(escape(s))
 
-let BLOCKS = true
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+let makeBlockAsCommandRelative(blockCmd,dx,dy,dz) =
+    if blockCmd = "R" then
+        sprintf "setblock ~%d ~%d ~%d redstone_block" dx dy dz
+    elif blockCmd = "P" then
+        sprintf "setblock ~%d ~%d ~%d repeating_command_block" dx dy dz
+    elif blockCmd.StartsWith("O ") then
+        sprintf "setblock ~%d ~%d ~%d command_block 0 replace {Command:\"%s\"}" dx dy dz (escape (blockCmd.Substring(2)))
+    elif blockCmd.StartsWith("C ") then
+        sprintf "setblock ~%d ~%d ~%d chain_command_block 8 replace {auto:1b,Command:\"%s\"}" dx dy dz (escape (blockCmd.Substring(2)))
+    else 
+        sprintf "setblock ~%d ~%d ~%d chain_command_block 0 replace {auto:1b,Command:\"%s\"}" dx dy dz (escape blockCmd)
 
-[<System.STAThreadAttribute>]
-do
+let minecartCommandRunner(cmds:string[]) =
+    let sb = new System.Text.StringBuilder()
+    // runs all cmds in 1 tick, 8 ticks after activation, leaves only original block
+    sb.Append(sprintf """summon FallingSand ~ ~0.55 ~ {Block:command_block,Time:1,TileEntityData:{Command:"summon MinecartCommandBlock ~ ~1.5 ~ {Command:\"%s\",Passengers:[""" (escape2 cmds.[0])) |> ignore
+    for i = 1 to cmds.Length-1 do
+        sb.Append(sprintf """{id:MinecartCommandBlock,Command:\"%s\"},""" (escape2 cmds.[i])) |> ignore
+    sb.Append("""{id:MinecartCommandBlock,Command:\"setblock ~ ~-2 ~ command_block 0 0 {auto:1b,Command:\\\"fill ~ ~ ~ ~ ~2 ~ air\\\"}\"},{id:MinecartCommandBlock,Command:\"kill @e[type=MinecartCommandBlock,r=5]\"}]}"},Passengers:[{id:FallingSand,Block:redstone_block,Time:1,Passengers:[{id:FallingSand,Block:activator_rail,Time:1}]}]}""") |> ignore
+    sb.ToString()
+
+// first run all runCmds, then place each blockCmds array hanging down in the sky, starting at same y/z but with increasing x
+let makeOneCommandBlockWith(runCmds:string[], blockCmds:string[][]) =
+    let finalCmds = ResizeArray()
+    finalCmds.AddRange(runCmds)
+    let maxLen = blockCmds |> Array.map (fun a -> a.Length) |> Array.max
+    for x = 0 to blockCmds.Length-1 do
+        for i = 0 to blockCmds.[x].Length-1 do
+            let cmd = makeBlockAsCommandRelative(blockCmds.[x].[i], x, 2 + maxLen - i, 0)
+            finalCmds.Add(cmd)
+    minecartCommandRunner(finalCmds.ToArray())
+
+let makeOneCommandBlock(cmds:string[]) =
     let mutable sb = new System.Text.StringBuilder()
     if not(cmds.[0].StartsWith("MINECART")) then
 #if RIDING        
@@ -233,29 +294,24 @@ do
         if cmds.[0].StartsWith("MINECART BLOCKS") then
             for i = 1 to cmds.Length-1 do
                 let dy = 10 + cmds.Length - i 
-                if cmds.[i] = "R" then
-                    cmds.[i] <- sprintf "setblock ~ ~%d ~ redstone_block" dy
-                elif cmds.[i] = "P" then
-                    cmds.[i] <- sprintf "setblock ~ ~%d ~ repeating_command_block" dy
-                elif cmds.[i].StartsWith("O ") then
-                    cmds.[i] <- sprintf "setblock ~ ~%d ~ command_block 0 replace {Command:\"%s\"}" dy (escape (cmds.[i].Substring(2)))
-                elif cmds.[i].StartsWith("C ") then
-                    cmds.[i] <- sprintf "setblock ~ ~%d ~ chain_command_block 8 replace {auto:1b,Command:\"%s\"}" dy (escape (cmds.[i].Substring(2)))
-                else 
-                    cmds.[i] <- sprintf "setblock ~ ~%d ~ chain_command_block 0 replace {auto:1b,Command:\"%s\"}" dy (escape cmds.[i])
-        // runs all cmds in 1 tick, 8 ticks after activation, leaves only original block
-        sb.Append(sprintf """summon FallingSand ~ ~0.55 ~ {Block:command_block,Time:1,TileEntityData:{Command:"summon MinecartCommandBlock ~ ~1.5 ~ {Command:\"%s\",Passengers:[""" (escape2 cmds.[1])) |> ignore
-        for i = 2 to cmds.Length-1 do
-            sb.Append(sprintf """{id:MinecartCommandBlock,Command:\"%s\"},""" (escape2 cmds.[i])) |> ignore
-        sb.Append("""{id:MinecartCommandBlock,Command:\"setblock ~ ~-2 ~ command_block 0 0 {auto:1b,Command:\\\"fill ~ ~ ~ ~ ~2 ~ air\\\"}\"},{id:MinecartCommandBlock,Command:\"kill @e[type=MinecartCommandBlock,r=5]\"}]}"},Passengers:[{id:FallingSand,Block:redstone_block,Time:1,Passengers:[{id:FallingSand,Block:activator_rail,Time:1}]}]}""") |> ignore
+                cmds.[i] <- makeBlockAsCommandRelative(cmds.[i],0,dy,0)
+        sb.Append(minecartCommandRunner(cmds.[1..])) |> ignore
 #endif
+    sb.ToString()
 
-    System.Windows.Clipboard.SetText(sb.ToString())
-    printfn "%s" (sb.ToString())
+[<System.STAThreadAttribute>]
+do
+//    let s = makeOneCommandBlock(cmds)
+    let s = makeOneCommandBlockWith([|"say run 1";"say run 2";"say run 3"|],
+                                    [| [|"P";"O say 1";"C say 1";"say 1";"R"|] 
+                                       [|"P";"O say 2";"C say 2";"say 2";"R"|] 
+                                    |])
+    System.Windows.Clipboard.SetText(s)
+    printfn "%s" (s)
     printfn ""
-    printfn "left  %d" (sb.ToString() |> Seq.filter (fun c -> c='{') |> Seq.length)
-    printfn "right %d" (sb.ToString() |> Seq.filter (fun c -> c='}') |> Seq.length)
-    printfn "chars %d" (sb.ToString().Length)
+    printfn "left  %d" (s |> Seq.filter (fun c -> c='{') |> Seq.length)
+    printfn "right %d" (s |> Seq.filter (fun c -> c='}') |> Seq.length)
+    printfn "chars %d" (s.Length)
     printfn "blocks %d" (cmds.Length)
     printfn ""
     printfn "it's now in your clipboard"
