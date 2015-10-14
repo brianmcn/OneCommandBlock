93c66bdefdec2c1af2dbddf204f23db33f91566e
brianmcn <lorgon1@yahoo.com>
2015-10-14 12:16:46 -0400

    Update Geometry.fs

--- Geometry.fs
+++ Geometry.fs
@@ -2,101 +2,98 @@ module Geometry
 
 let commonInit =
     [|
-        "R"
-        "O gamerule commandBlockOutput false"
-        "scoreboard objectives add LD dummy"     // LD is "lineDraw", but need short text to cram into command
+        "gamerule commandBlockOutput false"
+        "scoreboard objectives add lineDraw dummy"
         "scoreboard objectives add LDZrev dummy"
         "scoreboard objectives add LDYrev dummy"
         "scoreboard objectives add LDXrev dummy"
-        "scoreboard players set @a LD 0"
-        "scoreboard objectives add endpoint dummy"  // TODO clean up objectives
+        "scoreboard objectives add endpoint dummy"
+        "tp @p ~ ~110 ~"
     |]
 
-let cmdsDrawLine =
+let cmdsDrawLine = // assuming DX DY and DZ are set in lineDraw objective, then this draws the line from LDstart to relative (DX,DY,DZ) once LDstart exists
     [|
-        // assuming DX DY and DZ are set in LD objective, then this draws the line from LDs to relative (DX,DY,DZ) once LDs exists
-        yield "R"
-        yield "P"
         // deal with negative DX
-        yield "scoreboard players set @e[tag=LDs] LDXrev 0"   // LDs is LDstart, shortened
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players test DX LD * -1"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players set Temp LD 0"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players operation Temp LD -= DX LD"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players operation DX LD = Temp LD"
-        yield "C scoreboard players set @e[tag=LDs] LDXrev 1"
+        yield "P scoreboard players set @e[tag=LDstart] LDXrev 0"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players test DX lineDraw * -1"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players set Temp lineDraw 0"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation Temp lineDraw -= DX lineDraw"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation DX lineDraw = Temp lineDraw"
+        yield "C scoreboard players set @e[tag=LDstart] LDXrev 1"
         // deal with negative DY
-        yield "scoreboard players set @e[tag=LDs] LDYrev 0"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players test DY LD * -1"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players set Temp LD 0"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players operation Temp LD -= DY LD"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players operation DY LD = Temp LD"
-        yield "C scoreboard players set @e[tag=LDs] LDYrev 1"
+        yield "scoreboard players set @e[tag=LDstart] LDYrev 0"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players test DY lineDraw * -1"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players set Temp lineDraw 0"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation Temp lineDraw -= DY lineDraw"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation DY lineDraw = Temp lineDraw"
+        yield "C scoreboard players set @e[tag=LDstart] LDYrev 1"
         // deal with negative DZ
-        yield "scoreboard players set @e[tag=LDs] LDZrev 0"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players test DZ LD * -1"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players set Temp LD 0"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players operation Temp LD -= DZ LD"
-        yield "C execute @e[tag=LDs] ~ ~ ~ scoreboard players operation DZ LD = Temp LD"
-        yield "C scoreboard players set @e[tag=LDs] LDZrev 1"
+        yield "scoreboard players set @e[tag=LDstart] LDZrev 0"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players test DZ lineDraw * -1"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players set Temp lineDraw 0"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation Temp lineDraw -= DZ lineDraw"
+        yield "C execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation DZ lineDraw = Temp lineDraw"
+        yield "C scoreboard players set @e[tag=LDstart] LDZrev 1"
         // init vars, choose major axis
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TempZ LD = DX LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TempZ LD -= DZ LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players set @e[tag=LDs] LD 0"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation M LD = DZ LD"    // M is MAJOR axis length, shortened for command length
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players test TempZ LD 0 *"
-        yield "C scoreboard players set @e[tag=LDs] LD 1"
-        yield "C scoreboard players operation M LD = DX LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TempY LD = DY LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TempY LD -= M LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players test TempY LD 0 *"
-        yield "C scoreboard players set @e[tag=LDs] LD 2"
-        yield "C scoreboard players operation M LD = DY LD"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TempZ lineDraw = DX lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TempZ lineDraw -= DZ lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players set @e[tag=LDstart] lineDraw 0"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation MAJOR lineDraw = DZ lineDraw"  
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players test TempZ lineDraw 0 *"
+        yield "C scoreboard players set @e[tag=LDstart] lineDraw 1"
+        yield "C scoreboard players operation MAJOR lineDraw = DX lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TempY lineDraw = DY lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TempY lineDraw -= MAJOR lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players test TempY lineDraw 0 *"
+        yield "C scoreboard players set @e[tag=LDstart] lineDraw 2"
+        yield "C scoreboard players operation MAJOR lineDraw = DY lineDraw"
         // setup '2dx/2dy/2dz' vars
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TDZ LD = DZ LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TDZ LD += TDZ LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TDX LD = DX LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TDX LD += TDX LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TDY LD = DY LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TDY LD += TDY LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TM LD = M LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation TM LD += TM LD"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TDZ lineDraw = DZ lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TDZ lineDraw += TDZ lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TDX lineDraw = DX lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TDX lineDraw += TDX lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TDY lineDraw = DY lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TDY lineDraw += TDY lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TMAJOR lineDraw = MAJOR lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation TMAJOR lineDraw += TMAJOR lineDraw"
         // A = 2dz - dx
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation AZ LD = TDZ LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation AZ LD -= M LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation AY LD = TDY LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation AY LD -= M LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation AX LD = TDX LD"
-        yield "execute @e[tag=LDs] ~ ~ ~ scoreboard players operation AX LD -= M LD"
-        yield "entitydata @e[tag=LDs] {Tags:[\"LDloop\"]}"
-        yield "execute @e[tag=LDloop] ~ ~ ~ setblock ~ ~ ~ stone"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation AZ lineDraw = TDZ lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation AZ lineDraw -= MAJOR lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation AY lineDraw = TDY lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation AY lineDraw -= MAJOR lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation AX lineDraw = TDX lineDraw"
+        yield "execute @e[tag=LDstart] ~ ~ ~ scoreboard players operation AX lineDraw -= MAJOR lineDraw"
+        yield "entitydata @e[tag=LDstart] {Tags:[\"LDloop\"]}"
+        //yield "execute @e[tag=LDloop] ~ ~ ~ setblock ~ ~ ~ stone"
+        yield "execute @e[tag=LDloop] ~ ~ ~ summon AreaEffectCloud ~ ~ ~ {Duration:200,Tags:[\"lineDrawPlace\"]}"
         // loop
         // - diff on X
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AX LD 1 *"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AX lineDraw 1 *"
         yield "C tp @e[tag=LDloop,score_LDXrev=0] ~1 ~ ~"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AX LD 1 *"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AX lineDraw 1 *"
         yield "C tp @e[tag=LDloop,score_LDXrev_min=1] ~-1 ~ ~"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AX LD 1 *"
-        yield "C execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AX LD -= TM LD"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AX LD += TDX LD"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AX lineDraw 1 *"
+        yield "C execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AX lineDraw -= TMAJOR lineDraw"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AX lineDraw += TDX lineDraw"
         // - diff on Y
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AY LD 1 *"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AY lineDraw 1 *"
         yield "C tp @e[tag=LDloop,score_LDYrev=0] ~ ~1 ~"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AY LD 1 *"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AY lineDraw 1 *"
         yield "C tp @e[tag=LDloop,score_LDYrev_min=1] ~ ~-1 ~"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AY LD 1 *"
-        yield "C execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AY LD -= TM LD"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AY LD += TDY LD"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AY lineDraw 1 *"
+        yield "C execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AY lineDraw -= TMAJOR lineDraw"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AY lineDraw += TDY lineDraw"
         // - diff on Z
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AZ LD 1 *"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AZ lineDraw 1 *"
         yield "C tp @e[tag=LDloop,score_LDZrev=0] ~ ~ ~1"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AZ LD 1 *"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AZ lineDraw 1 *"
         yield "C tp @e[tag=LDloop,score_LDZrev_min=1] ~ ~ ~-1"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AZ LD 1 *"
-        yield "C execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AZ LD -= TM LD"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AZ LD += TDZ LD"
-        // endloop, stop when done after M steps
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players remove M LD 1"
-        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test M LD * -1"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test AZ lineDraw 1 *"
+        yield "C execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AZ lineDraw -= TMAJOR lineDraw"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players operation AZ lineDraw += TDZ lineDraw"
+        // endloop, stop when done after MAJOR steps
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players remove MAJOR lineDraw 1"
+        yield "execute @e[tag=LDloop] ~ ~ ~ scoreboard players test MAJOR lineDraw * -1"
         yield "C kill @e[tag=LDloop]"
     |]
 
@@ -105,10 +102,9 @@ let cmdsComputeDxDyDz =
         // computes the dx/dy/dz from A to B (provided it's in within -127...127) 
         let MAX = 128
         let STEPS = [-128;-64;-32;-16;-8;-4;-2;-1]
-        yield "O "
-        yield sprintf "scoreboard players set DX LD %d" (MAX-1)
-        yield sprintf "scoreboard players set DY LD %d" (MAX-0)  // y testing is weird for some reason...
-        yield sprintf "scoreboard players set DZ LD %d" (MAX-1)
+        yield sprintf "O scoreboard players set DX lineDraw %d" (MAX-1)
+        yield sprintf "scoreboard players set DY lineDraw %d" (MAX-0)  // y testing is weird for some reason...
+        yield sprintf "scoreboard players set DZ lineDraw %d" (MAX-1)
         yield sprintf "execute @e[tag=LDA] ~ ~ ~ summon ArmorStand ~%d ~ ~ {Marker:1,NoGravity:1,Tags:[\"LDTX\"]}" MAX
         yield sprintf "execute @e[tag=LDA] ~ ~ ~ summon ArmorStand ~ ~%d ~ {Marker:1,NoGravity:1,Tags:[\"LDTY\"]}" MAX
         yield sprintf "execute @e[tag=LDA] ~ ~ ~ summon ArmorStand ~ ~ ~%d {Marker:1,NoGravity:1,Tags:[\"LDTZ\"]}" MAX
@@ -116,42 +112,77 @@ let cmdsComputeDxDyDz =
             yield sprintf "execute @e[tag=LDTX] ~%d ~%d ~%d testfor @e[tag=LDB,dx=%d,dy=%d,dz=%d]" (0) (0-MAX) (0-MAX) (s) (2*MAX) (2*MAX)
             yield "testforblock ~ ~1 ~ chain_command_block -1 {SuccessCount:0}"
             yield sprintf "C tp @e[tag=LDTX] ~%d ~%d ~%d" s 0 0
-            yield sprintf "C scoreboard players remove DX LD %d" (-s)
+            yield sprintf "C scoreboard players remove DX lineDraw %d" (-s)
             
             yield sprintf "execute @e[tag=LDTY] ~%d ~%d ~%d testfor @e[tag=LDB,dx=%d,dy=%d,dz=%d]" (0-MAX) (0) (0-MAX) (2*MAX) (s) (2*MAX)
             yield "testforblock ~ ~1 ~ chain_command_block -1 {SuccessCount:0}"
             yield sprintf "C tp @e[tag=LDTY] ~%d ~%d ~%d" 0 s 0
-            yield sprintf "C scoreboard players remove DY LD %d" (-s)
+            yield sprintf "C scoreboard players remove DY lineDraw %d" (-s)
 
             yield sprintf "execute @e[tag=LDTZ] ~%d ~%d ~%d testfor @e[tag=LDB,dx=%d,dy=%d,dz=%d]" (0-MAX) (0-MAX) (0) (2*MAX) (2*MAX) (s)
             yield "testforblock ~ ~1 ~ chain_command_block -1 {SuccessCount:0}"
             yield sprintf "C tp @e[tag=LDTZ] ~%d ~%d ~%d" 0 0 s
-            yield sprintf "C scoreboard players remove DZ LD %d" (-s)
+            yield sprintf "C scoreboard players remove DZ lineDraw %d" (-s)
         yield "kill @e[tag=LDTX]"
         yield "kill @e[tag=LDTY]"
         yield "kill @e[tag=LDTZ]"
         // call the draw-er
-        yield "entitydata @e[tag=LDA] {Tags:[\"LDs\"]}"
+        yield "entitydata @e[tag=LDA] {Tags:[\"LDstart\"]}"
         yield "kill @e[tag=LDB]"
     |]
 
 let kickStart = 
     [|
-        "R"
-        "P"
         // if A placed, and player demands second endpoint, then place it and run the tool
-        "execute @e[tag=LDA] ~ ~ ~ scoreboard players test @p endpoint 1 *"
+        "P execute @e[tag=LDA] ~ ~ ~ scoreboard players test @p endpoint 1 1"
+#if SHULKER
+        "C execute @p ~ ~ ~ summon Shulker ~ ~ ~ {NoAI:1}"  // snap to grid
+        "C execute @e[type=Shulker] ~ ~ ~ summon ArmorStand ~ ~ ~ {Marker:1,NoGravity:1,Tags:[\"LDB\"]}"
+#else
         "C execute @p ~ ~ ~ summon ArmorStand ~ ~ ~ {Marker:1,NoGravity:1,Tags:[\"LDB\"]}"
-        "C blockdata ~2 ~4 ~ {auto:1b}"  // TODO coords - call out to compute dxdydz
-        "C blockdata ~2 ~5 ~ {auto:0b}"
+#endif
+        "C blockdata ~2 ~2 ~ {auto:1b}"  // TODO coords - call out to compute dxdydz
+        "C blockdata ~2 ~3 ~ {auto:0b}"
         // if A not yet placed, and player demands an endpoint, place A"
         "testfor @e[tag=LDA]"
         "testforblock ~ ~1 ~ chain_command_block -1 {SuccessCount:0}"
-        "C scoreboard players test @p endpoint 1 *"
+        "C scoreboard players test @p endpoint 1 1"
+#if SHULKER
+        "C execute @p ~ ~ ~ summon Shulker ~ ~ ~ {NoAI:1}"  // snap to grid
+        "C execute @e[type=Shulker] ~ ~ ~ summon ArmorStand ~ ~ ~ {Marker:1,NoGravity:1,Tags:[\"LDA\"]}"
+        "kill @e[type=Shulker]"
+#else
         "C execute @p ~ ~ ~ summon ArmorStand ~ ~ ~ {Marker:1,NoGravity:1,Tags:[\"LDA\"]}"
+#endif
+        // make barrier particles
+        "execute @e[tag=lineDrawPlace] ~ ~ ~ particle barrier ~ ~ ~ 0 0 0 0 0 force"
+        // if player wants, convert to block he stands on
+        "scoreboard players test @p endpoint 2 2"
+        """C execute @e[tag=lineDrawPlace] ~ ~ ~ setblock ~ ~ ~ stone"""
+        //"""C execute @e[tag=lineDrawPlace] ~ ~ ~ setblock ~ ~ ~ command_block 0 replace {auto:1b,Command:"execute @p ~ ~ ~ ???"}"""  // TODO block type with clone
+        "C kill @e[tag=lineDrawPlace]"
         // clear endpoint request
         "scoreboard players set @p endpoint 0"
-        // TODO make barrier particles
     |]
 
-let All = (commonInit, [| kickStart; cmdsComputeDxDyDz; cmdsDrawLine |])
+let shorten(a:string[]) =
+    // in order to fit into one command block, need to shorten some identifiers
+    a 
+    |> Array.map (fun s -> s.Replace("MAJOR","M"))
+    |> Array.map (fun s -> s.Replace("LDstart","J"))
+    |> Array.map (fun s -> s.Replace("LDloop","R"))
+    |> Array.map (fun s -> s.Replace("LDXrev","XR"))
+    |> Array.map (fun s -> s.Replace("LDYrev","YR"))
+    |> Array.map (fun s -> s.Replace("LDZrev","ZR"))
+    |> Array.map (fun s -> s.Replace("Temp","Tm"))
+    |> Array.map (fun s -> s.Replace("LDTX","B"))
+    |> Array.map (fun s -> s.Replace("LDTY","C"))
+    |> Array.map (fun s -> s.Replace("LDA","F"))
+    |> Array.map (fun s -> s.Replace("LDB","G"))
+    |> Array.map (fun s -> s.Replace("lineDrawPlace","H"))
+    |> Array.map (fun s -> s.Replace("lineDraw","Q"))
+
+// TODO note that can save a lot by changing MAX/STEPS to 64 instead of 128 if needed
+// TODO could auto:1b the purples to avoid needing the reds
+
+let All = (shorten commonInit, [| shorten kickStart; shorten cmdsComputeDxDyDz; shorten cmdsDrawLine |])
